ac3d4b985b57ffce3994998c4f0bf7940cb417e0
Joshua Cook <me@joshuacook.me>
2016-08-26 18:58:38 +0000

    update dotfiles

--- push.sh
+++ push.sh
@@ -3,37 +3,7 @@ cp ~/.bash_profile .bash_profile
 rm -rf .bash 
 cp -R ~/.bash .bash
 cp ~/.vimrc .vimrc
-rm -rf .vim
-cp -R ~/.vim .vim
 
-if [ "$1" == "" ]; then
-  git add .
-  git commit -m 'update dotfiles'
-  git push
-fi  
-
-
-while [ "$1" != "" ] ; do
-  case "$1" in
-    aliases)
-       git add .bash/.bash_a*
-       git commit -m 'update aliases'
-       git push
-       ;;
-    functions)
-       git add .bash/.bash_f*
-       git commit -m 'update functions'
-       git push
-       ;;
-    vim)
-       git add .vim .vimrc
-       git commit -m 'update vim'
-       git push
-       ;;
-    *)
-       error $1
-       exit -1
-       ;;
-  esac
-  shift
-done
+git add -A 
+git commit -m 'update dotfiles'
+git push
