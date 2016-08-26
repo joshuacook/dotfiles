b4b46dd3b08fcb082f0a1dc04938236754b4f971
Joshua Cook <me@joshuacook.me>
2016-08-26 18:48:09 +0000

    update load script

--- bash_load.sh
+++ bash_load.sh
@@ -5,6 +5,13 @@ rm -rf ~/.bash
 cp -R .bash ~/.bash
 cp .vimrc ~/.vimrc
 rm -rf ~/.vim
-cp -R .vim ~/.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+git clone git@github.com:ctrlpvim/ctrlp.vim.git
+git clone git@github.com:ervandew/supertab.git
+git clone git@github.com:majutsushi/tagbar.git
+git clone git@github.com:vim-airline/vim-airline.git
+git clone git@github.com:tpope/vim-sensible.git
 source ~/.bash_profile
-cd
+cd -                                                                                                                                    
