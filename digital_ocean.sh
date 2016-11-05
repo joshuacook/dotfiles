a7786591054cf3b2b86d1ed0afe05cfb133f21f5
Joshua Cook <me@joshuacook.me>
2016-11-05 16:14:27 -0700

    Update digital_ocean.sh

--- digital_ocean.sh
+++ digital_ocean.sh
@@ -7,3 +7,5 @@ cp /home/joshuacook/.ssh/authorized_keys /home/joshuacook/.ssh/id_rsa
 # copy logins from lastpass
 curl -L https://github.com/docker/machine/releases/download/v0.8.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && chmod +x /usr/local/bin/docker-machine
 sudo apt-get install parallel
+# sudo vi /etc/default/docker
+# DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock"
