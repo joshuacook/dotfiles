c38bd8c6647f2424684a4d7a0c9561a111f771f2
Joshua Cook <me@joshuacook.me>
2016-11-05 15:31:11 -0700

    Update and rename adduser.sh to digital_ocean.sh

--- digital_ocean.sh
+++ digital_ocean.sh
@@ -4,3 +4,5 @@ usermod -aG docker joshuacook
 cp -r /root/.ssh/ /home/joshuacook/
 chown joshuacook:joshuacook /home/joshuacook/.ssh/
 cp /home/joshuacook/.ssh/authorized_keys /home/joshuacook/.ssh/id_rsa
+# copy logins from lastpass
+curl -L https://github.com/docker/machine/releases/download/v0.8.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && chmod +x /usr/local/bin/docker-machine
