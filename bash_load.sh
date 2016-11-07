e8620042e72babad603c8fd73ba7b6387c5ae576
Joshua Cook <me@joshuacook.me>
2016-11-07 19:56:45 +0000

    note tool

--- bash_load.sh
+++ bash_load.sh
@@ -8,10 +8,11 @@ rm -rf ~/.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
-git clone git@github.com:ctrlpvim/ctrlp.vim.git
-git clone git@github.com:ervandew/supertab.git
-git clone git@github.com:majutsushi/tagbar.git
-git clone git@github.com:vim-airline/vim-airline.git
-git clone git@github.com:tpope/vim-sensible.git
+https://github.com/joshuacook/dotfiles.git
+git clone https://github.com/ctrlpvim/ctrlp.vim.git
+git clone https://github.com/ervandew/supertab.git
+git clone https://github.com/majutsushi/tagbar.git
+git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/tpope/vim-sensible.git
 source ~/.bash_profile
 cd -                                                                                                                                    
