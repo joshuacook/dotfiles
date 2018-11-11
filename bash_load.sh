9f932e13793a0e259ce7ee61b3db3d31b8cc80c8
Joshua Cook <joshuacook@Joshuas-iMac.local>
2018-11-11 09:26:07 -0800

    latest

--- bash_load.sh
+++ bash_load.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-git pull
 cp .bash_profile ~/.bash_profile
-rm -rf ~/.bash
-cp -R .bash ~/.bash
 cp .vimrc ~/.vimrc
 rm -rf ~/.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
-https://github.com/joshuacook/dotfiles.git
 git clone https://github.com/ctrlpvim/ctrlp.vim.git
 git clone https://github.com/ervandew/supertab.git
 git clone https://github.com/majutsushi/tagbar.git
--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cp ~/.bash_profile .bash_profile
-rm -rf .bash 
-cp -R ~/.bash .bash
-cp ~/.vimrc .vimrc
-
-git add -A 
-git commit -m 'update dotfiles'
-git push
