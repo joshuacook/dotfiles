75b2c08410c4752bc8dddfb62778871b7e01ee88
Joshua Cook <me@joshuacook.me>
2019-01-01 10:35:04 -0800

    Update aws-boot.sh

--- aws-boot.sh
+++ aws-boot.sh
@@ -6,5 +6,5 @@ cd
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker ubuntu
 sudo docker pull jupyter/datascience-notebook
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
