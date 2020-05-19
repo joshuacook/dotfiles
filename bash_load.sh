6e2afee03ddc4bf37d7b349eff69da5ae8acaa69
Joshua Cook <joshua.cook@databricks.com>
2020-05-19 11:09:50 -0700

    Update bash_load.sh

--- bash_load.sh
+++ bash_load.sh
@@ -5,6 +5,7 @@ rm -rf ~/.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
+git clone https://github.com/mattn/emmet-vim.git ~/.vim/bundle/emmet-vim
 git clone https://github.com/ctrlpvim/ctrlp.vim.git
 git clone https://github.com/ervandew/supertab.git
 git clone https://github.com/majutsushi/tagbar.git
